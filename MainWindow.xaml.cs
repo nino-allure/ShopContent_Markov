a04d6cfcb0af098036a62f1d13107c2b5ad1e588
DikiYLexa <dubovan36@gmail.com>
2026-04-25 15:27:06 +0500

    Разработанная главная страница. markov

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,15 @@ namespace ShopContent_Markov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public View.Main Main = new View.Main();
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            frame.Navigate(Main);
         }
+        private void OpenIndex(object sender, MouseButtonEventArgs e) =>
+            frame.Navigate(Main);
     }
 }
