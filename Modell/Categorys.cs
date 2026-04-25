d8064d5a94790657634bd09ab0edefbdd04ae4fb
DikiYLexa <dubovan36@gmail.com>
2026-04-25 15:50:38 +0500

    Разработанные модели данных. markov

---
+++ Modell/Categorys.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent_Markov.Modell
+{
+    public class Categorys : INotifyPropertyChanging
+    {
+        private int id;
+        public int Id { 
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
