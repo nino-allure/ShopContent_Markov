728848748b3ea1fadda4a9a66f84b16af4e0fab3
DikiYLexa <dubovan36@gmail.com>
2026-04-25 16:42:26 +0500

    Созданная страница добавления и изменения. markov

---
+++ View/Add.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShopContent_Markov.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        public Add(object Context)
+        {
+            InitializeComponent();
+            DataContext = new
+            {
+                item = Context,
+                categorys = new ViewModell.VMCategorys()
+            };
+        }
+    }
+}
